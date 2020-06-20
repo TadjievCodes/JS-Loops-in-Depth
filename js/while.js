d95bfd606d60e561488da7c2a52aad145dee89be
TadjievCodes <muhammad2008.08@mail.ru>
2020-06-20 19:16:48 -0400

    while loop with let

--- js/while.js
+++ js/while.js
@@ -3,6 +3,12 @@ function getRandomNumber(upper) {
 }
 
 
+// Remember, the value of a variable declared with const cannot be changed through reassignment. 
+// We're using the addition assignment operator (+=) to add one to the current value of counter, until it reaches the value 10
+
+
+
+
 // Counter is the variable that gets incremented every time
 
 let counter = 0;
@@ -11,3 +17,6 @@ while (counter < 100) {
     console.log(`The random number is ${getRandomNumber(10)} `);
     counter += 1;
 }
+
+
+// In this case, it's important to use the keyword let, which, as you learned, lets you change (or manipulate) a variable's value through reassignment
