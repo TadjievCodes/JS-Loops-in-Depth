2d12d896250f793873351889dd37a6e30852da81
TadjievCodes <muhammad2008.08@mail.ru>
2020-06-20 20:37:12 -0400

    for loop another

--- js/for.js
+++ js/for.js
@@ -11,3 +11,13 @@ for (let counter = 0; counter < 10; counter += 1) { // Or counter ++
     console.log(`The random number is ${getRandomNumber(10)} `);
 
 }
+
+
+// Another example so it generates numbers from 5 up to 100 and += 1 so incrmenents once every number
+
+// So counter starts from 5
+
+
+for (let counter = 5; counter <= 100; counter += 1) {
+    console.log(counter);
+}
