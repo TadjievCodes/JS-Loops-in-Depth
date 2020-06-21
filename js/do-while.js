ebc79864da3360a678f4afd9313c92717cb183ef
TadjievCodes <muhammad2008.08@mail.ru>
2020-06-20 20:12:42 -0400

    another example of do while

--- js/do-while.js
+++ js/do-while.js
@@ -51,3 +51,20 @@ do {
 This time, the loop counts down from 10 to 0, decreasing the value assigned to the counter variable by one each time through the loop. It's the same as counter -= 1.
 
 */
+
+//** Another Do While Loop Example
+
+
+// Display the prompt dialogue while the value assigned to `secret` is not equal to "sesame"
+
+let secret;
+
+do {
+    secret = prompt("What is the secret password?");
+
+
+
+} while (secret !== "sesame");
+
+// This should run after the loop is done executing
+alert("You know the secret password. Welcome!");
