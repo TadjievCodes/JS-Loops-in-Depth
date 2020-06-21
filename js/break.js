6b8d48c8eb1afd00a308aa358eebb0452d5b44c5
TadjievCodes <muhammad2008.08@mail.ru>
2020-06-20 21:30:06 -0400

    i-- practice

--- js/break.js
+++ js/break.js
@@ -2,10 +2,12 @@ const secretWord = 'tomato';
 let message = 'Access denied :(';
 
 for (let i = 5; i >= 1; i--) {
+    // And because of this i-- it keeps showing us how many tries we have left after the loop ran, cool thing 
     let guess = prompt(`Enter the secret word. You have ${i} tries.`);
     if (guess === secretWord) {
         message = 'Welcome to the secret loop world!';
         break; // immediately terminate the loop
+        // So that it doesn't keep running but exits the loop
     }
 }
 
